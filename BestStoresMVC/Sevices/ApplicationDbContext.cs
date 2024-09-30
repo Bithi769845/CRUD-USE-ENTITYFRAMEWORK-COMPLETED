fed47c7f5afe7bd34213ce14432bc7dc223f6579
Ms.Bithi <bithimony01904@gmail.com>
2024-09-30 17:24:35 +0600

    CRUD is done

---
+++ BestStoresMVC/Sevices/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BestStoresMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoresMVC.Sevices
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
